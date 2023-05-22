c0b94b5f24e4f14ec29710a9ad57fc4a4c0049e4
n2ai <trinhh7@gator.uhd.edu>
2023-05-22 11:17:52 +0700

    User Register done

--- api/index.js
+++ api/index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const User = require('./models/User')
+const bcrypt = require('bcryptjs')
+
+const bcryptSalt = bcrypt.genSaltSync(10);
 require('dotenv').config()
 
-app.use(cors())
+app.use(cors({
+    credentials:true,
+    origin: 'http://127.0.0.1:5173',
+}))
 app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URL)
@@ -13,11 +20,15 @@ app.get('/test', (req,res)=>{
     res.json('test ok')
 });
 
-app.post('/register', (req,res)=>{
+app.post('/register', async (req,res)=>{
     const{name,email,password} = req.body;
-    
+    const userDoc = await User.create({
+        name,
+        email,
+        password:bcrypt.hashSync(password, bcryptSalt)
+    });
 
-    res.json(name,email,password);
+    res.json(userDoc);
 })
 
 app.listen(4000)
