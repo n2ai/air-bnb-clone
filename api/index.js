f2ce8efd7a0dcede3253e234361ac4c6edaea3ae
n2ai <trinhh7@gator.uhd.edu>
2023-05-24 15:56:20 +0700

    Profile update

--- api/index.js
+++ api/index.js
@@ -5,7 +5,9 @@ const mongoose = require('mongoose')
 const User = require('./models/User')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser');
 
+app.use(cookieParser())
 const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = 'adasdasdasdasd'
 require('dotenv').config()
@@ -41,24 +43,38 @@ app.post('/register', async (req,res)=>{
 })
 
 app.post('/login', async (req,res)=>{
-    const{email,password} = req.body;
-    const userDoc = await User.findOne({email});
-    if(userDoc){
-        const passOK = bcrypt.compareSync(password,userDoc.password);
-        if(passOK){
-            jwt.sign({email:userDoc.email, id:userDoc._id},jwtSecret,{},(err,token)=>{
-                if(err) throw err;
-                res.cookie('token',token).json('pass ok')
-            } )
-            
-        }else{
-            res.status(422).json('pass not ok')
-        }
-        
-    }else{
-        res.json('not found')
-        
+  mongoose.connect(process.env.MONGO_URL);
+  const {email,password} = req.body;
+  const userDoc = await User.findOne({email});
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign({
+        email:userDoc.email,
+        id:userDoc._id,
+        name:userDoc.name,
+      }, jwtSecret, {}, (err,token) => {
+        if (err) throw err;
+        res.cookie('token', token).json(userDoc);
+      });
+    } else {
+      res.status(422).json('pass not ok');
     }
+  } else {
+    res.json('not found');
+  }
+})
+
+app.get('/profile', (req,res)=>{
+  const {token} = req.cookies;
+  if(token){
+    jwt.verify(token,jwtSecret, {}, (err,user)=>{
+      if (err) throw err;
+      res.json(user)
+    } )
+  }else{
+    res.json(null)
+  }
 })
 
 app.listen(4000)
