0e2e2651f2e01d53bdd3790f1a32657dec99ca6c
n2ai <trinhh7@gator.uhd.edu>
2023-05-28 22:45:42 +0700

    Adding accomodations section

---
+++ api/models/Place.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const PlaceSchema = new mongoose.Schema({
+    owner:{type:mongoose.Schema.Types.ObjectId, ref:'User'},
+    title:String,
+    address: String,
+    photos: [String],
+    description: String,
+    perks: [String],
+    extraInfo: String,
+    checkIn: Number,
+    maxGuest: Number,
+});
+
+const PlaceModel = mongoose.model('Place', PlaceSchema);
+
+module.exports =  PlaceModel;
