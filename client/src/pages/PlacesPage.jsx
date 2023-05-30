f8cb8f32e07aa932803b63519f8be00f0e5e9ff2
n2ai <trinhh7@gator.uhd.edu>
2023-05-30 18:49:19 +0700

    Refactor PlacesPage.jsx

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,8 +1,40 @@
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom"
 
 export default function PlacesPage(){
     const {action} = useParams();
-    console.log(action)
+    const [title,setTitle] = useState();
+    const [address, setAddress] = useState('');
+    const [addedPhotos, setAddedPhotos] = useState([]);
+    const [photoLink, setPhotoLink] = useState('');
+    const [description, setDescription] = useState('');
+    const [perks, setPerks] = useState([]);
+    const [extraInfo, setExtraInfo] = useState('');
+    const [checkIn, setCheckIn] = useState('');
+    const [checkOut, setCheckOut] = useState('');
+    const [maxGuests, setMaxGuests] = useState(1);
+
+    function inputHeader(text){
+        return (
+            <h2 className="text-2xl mt-4">{text}</h2>
+        )
+    }
+
+    function inputDescription(text){
+        return (
+            <p className="text-gray-500 text-sm">{text}</p>
+        )
+    }
+
+    function preInput(header,description){
+        return (
+            <>
+                {inputHeader(header)}
+                {inputDescription(description)}
+            </>
+        )
+    }
+
     return(
         <div>
             {action !== 'new' && (
@@ -18,14 +50,11 @@ export default function PlacesPage(){
             {action == 'new' &&(
                 <div>
                     <form>
-                        <h2 className="text-2xl mt-4">Tittle</h2>
-                        <p className="text-gray-500 text-sm">title for your place, should be short and catchy as in advertisement</p>
+                        {preInput('Title','title for your place, should be short and catchy as in advertisement')}
                         <input type="text" placeholder="title, for example: My lovely apt"></input>
-                        <h2 className="text-2xl mt-4">Address</h2>
-                        <p className="text-gray-500 text-sm">Address to this place</p>
+                        {preInput('Address','Address to this place' )}
                         <input type="text" placeholder="address"></input>
-                        <h2 className="text-2xl mt-4">Photos</h2>
-                        <p className="text-gray-500 text-sm">More = better</p>
+                        {preInput('Photos', 'More = better')}
                         <div className="flex gap-2">
                             <input type="text" placeholder={'Add using a link....jpg'} />
                             <button className="bg-gray-200 px-4 rounded-2xl">Add&nbsp; photo</button>
@@ -38,8 +67,7 @@ export default function PlacesPage(){
                                 Upload
                             </button>
                         </div>
-                        <h2 className="text-2xl mt-4">Description</h2>
-                        <p className="text-gray-500 text-sm">description of the place</p>
+                        {preInput('Description', 'description of the place')}
                         <textarea></textarea>
                         <h2 className="text-2xl mt-4">Perks</h2>
                         <p className="text-gray-500 text-sm">Select all the perks of the place</p>
@@ -84,13 +112,29 @@ export default function PlacesPage(){
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 7.5h-.75A2.25 2.25 0 004.5 9.75v7.5a2.25 2.25 0 002.25 2.25h7.5a2.25 2.25 0 002.25-2.25v-7.5a2.25 2.25 0 00-2.25-2.25h-.75m-6 3.75l3 3m0 0l3-3m-3 3V1.5m6 9h.75a2.25 2.25 0 012.25 2.25v7.5a2.25 2.25 0 01-2.25 2.25h-7.5a2.25 2.25 0 01-2.25-2.25v-.75" />
                                 </svg>
-                                <span>Entrance</span>
+                                <span>Private entrance</span>
                             </label>
                         </div>
-                        
-                        <h2 className="text-2xl mt-4">Extra info</h2>
-                        <p className="text-gray-500 text-sm">house rules, etc</p>
+                        {preInput('Extra info','house rules, etc')}
                         <textarea></textarea>
+                        {preInput('Check in&out times. max guest','add check in and out times, remember to have some time window for cleaning the room between guests')}
+                        <div className="grid gap-2 sm:grid-cols-3">
+                            <div>
+                                <h3 className="mt-2 -mb-1">Check in time</h3>
+                                <input type="text" placeholder="14:00"></input>
+                            </div>
+                            <div>
+                                <h3 className="mt-2 -mb-1">Check out time</h3>
+                                <input type="text"></input>
+                            </div>
+                            <div>
+                                <h3 className="mt-2 -mb-1">Max number of guests</h3>
+                                <input type="text"></input>
+                            </div>
+                        </div>
+                        <div>
+                            <button className="primary my-4">Save</button>
+                        </div>
                     </form>
                 </div>
             )}
